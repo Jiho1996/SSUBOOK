a061b3b7d5b0dd186c5b7241103e772b4df94d0b
jinook518 <jinook517@gmail.com>
2020-12-13 05:08:44 +0900

    Feed에 SwipeRefreshLayout 적용

--- app/src/main/java/org/techtown/ssubook/Feed.java
+++ app/src/main/java/org/techtown/ssubook/Feed.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 import android.view.ViewGroup;
@@ -30,6 +31,7 @@ public class Feed extends AppCompatActivity
     private RecyclerView.Adapter feedAdapter;
     private RecyclerView.LayoutManager feedManager;
     private ActionBar feedActionbar;
+    private SwipeRefreshLayout swipeLayout;
     ArrayList<BookItem> bookItemBundle = new ArrayList<>();
 
     @Override
@@ -38,6 +40,7 @@ public class Feed extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
 
+        swipeLayout = (SwipeRefreshLayout)findViewById(R.id.swipe_layout);
         feedRecyclerView = (RecyclerView) findViewById(R.id.recyclerView_feed);
         feedActionbar = getSupportActionBar();
 
@@ -121,6 +124,51 @@ public class Feed extends AppCompatActivity
                 }
             });
         }
+
+        swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener()
+        {
+            @Override
+            public void onRefresh()
+            {
+                //데이터 읽어오기
+                FirebaseFirestore firebaseDB = FirebaseFirestore.getInstance();
+                firebaseDB.collection("Post").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task)
+                    {
+                        if (task.isSuccessful())
+                        {
+                            for (QueryDocumentSnapshot document : task.getResult()) //Task 종료 시 getResult는 QuerySnapShot을 return, QuerySnapShot은 Iterable이므로 for-each 문으로 QueryDocumentSnapshot으로 사용가능.
+                            {
+                                //QueryDocumentSnapshot은 모두 document형, getData()로 Map<String,Object>를 return
+                                Map<String,Object> dataMap = document.getData();
+                                String title = dataMap.get("title").toString();   //제목
+                                String author = dataMap.get("author").toString();  //use firebase UID, 저자
+                                String UID = dataMap.get("UID").toString(); //게시글 UID
+                                int price = (int)dataMap.get("price");  //가격
+                                Object timeStamp_o =  dataMap.get("timeStamp");
+                                long timeStamp = ((Timestamp) timeStamp_o).getSeconds();
+
+
+                                //책 상태
+                                String underbarTrace = dataMap.get("underbarTrace").toString();   //NONE, PENCIL, PEN
+                                String writeTrace = dataMap.get("writeTrace").toString();  //NONE, PENCIL, PEN
+                                String bookCover = dataMap.get("bookCover").toString(); //CLEAN, DIRTY
+                                boolean naming = (boolean)dataMap.get("naming"); //true:이름있음, false:없음
+                                boolean discolor = (boolean)dataMap.get("discolor");   //변색, true:있음, false:없음
+                                bookItemBundle.add(new BookItem(title,author,UID,price,timeStamp,underbarTrace,writeTrace,bookCover,naming,discolor));
+                            }
+                        }
+                        else
+                        {
+                            //작업 실패 시
+                        }
+                    }
+                });
+                swipeLayout.setRefreshing(false);
+            }
+        });
 
 
 
