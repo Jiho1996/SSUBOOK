25af92bf4749f8464fddb2828ed189beaddff675
jinook518 <jinook517@gmail.com>
2020-12-13 21:11:43 +0900

    Finish

--- app/src/main/java/org/techtown/ssubook/ServiceActivity.java
+++ app/src/main/java/org/techtown/ssubook/ServiceActivity.java
@@ -2,13 +2,16 @@ package org.techtown.ssubook;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ServiceActivity extends AppCompatActivity {
-
+    private ActionBar actionBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+        actionBar = getSupportActionBar();
+        actionBar.hide();
     }
 }
